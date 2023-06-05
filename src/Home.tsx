792e2aed384c46300112876669ce5676baee4495
juanlucasumali <juanlucasumali@berkeley.edu>
2023-06-05 14:31:45 +0800

    background made px

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,22 +37,26 @@ const Home: React.FC = () => {
   return (
     <div>
 
-    <div className="background">
-      <div className="orbits">
-      <div className="outer-orbit"></div>
-      <div className="inner-orbit"></div>
+      <div className="background">
+        <div className="orbits">
+          <div className="outer-orbit"></div>
+          <div className="inner-orbit"></div>
+        </div>
+        <div className='saturn'>
+          <div className='saturn-body'></div>
+          <div className='saturn-rings'></div>
+        </div>
       </div>
-    </div>
 
-    <div className="home-headers">
-      <div className='main-header'>
-      <h1 className="main-header-emoji">👋🏽</h1>
-      <h1 className="main-header-greeting">kamusta!</h1>
+      <div className="home-headers">
+        <div className='main-header'>
+        <h1 className="main-header-emoji">👋🏽</h1>
+        <h1 className="main-header-greeting">kamusta!</h1>
+        </div>
+        <h2 className="sub-header">
+          i’m juan lucas, a computer science major @ uc berkeley
+        </h2>
       </div>
-      <h2 className="sub-header">
-        i’m juan lucas, a computer science major @ uc berkeley
-      </h2>
-    </div>
 
     </div>
   );
