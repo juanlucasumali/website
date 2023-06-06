0110a2cf0aa0199bf3309461637d329a24344765
juanlucasumali <juanlucasumali@berkeley.edu>
2023-06-07 03:50:05 +0800

    navbar on right now

--- src/App.tsx
+++ src/App.tsx
@@ -20,107 +20,73 @@ import Button from '@mui/material/Button';
 const drawerWidth = 240;
 const navItems = ['👋🏽', '💻', '🎞'];
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-function App(props: Props) {
-
-  const { window } = props;
+function App() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+    setMobileOpen(!mobileOpen);
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle}>
       <List>
         {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-          <ListItemButton>
-        <ListItemText
-          primary={
-            <Typography variant="h6" style={{ fontSize: 30 }} sx={{ textAlign: 'center' }}>
-              {item}
-            </Typography>
-          }
-        />
-      </ListItemButton>
+          <ListItem key={item}>
+            <ListItemButton>
+              <ListItemText
+                primary={
+                  <Typography variant="h6" sx={{ fontSize: 30, textAlign: 'center'}}>
+                    {item}
+                  </Typography>
+                }
+              />
+            </ListItemButton>
           </ListItem>
         ))}
       </List>
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
-
   return (
     <div>
-
-<Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ right: 0, backgroundColor: 'transparent'}} elevation={0} position="fixed" >
-        <Toolbar>
+      <AppBar position="fixed" sx={{ backgroundColor: 'transparent', boxShadow: 'none', right: 0 }}>
+        <Toolbar sx={{ justifyContent: 'flex-end' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            edge="start"
+            edge="end"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, color: '#212121' }}
+            sx={{ ml: 2, color: '#212121' }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none'} }}
-          >
-            MUI
-          </Typography>
-          <Box sx={{ display: { xs: 'none'} }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block'},
-            '& .MuiDrawer-paper': { 
-              boxSizing: 'border-box', 
-              width: drawerWidth,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+      <Drawer
+        variant="temporary"
+        anchor="right"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          },
+        }}
+      >
+        {drawer}
+      </Drawer>
       <Box component="main" sx={{ p: 3 }}>
         <Home />
       </Box>
-    </Box>
-
     </div>
   );
-};
+}
 
 export default App;
